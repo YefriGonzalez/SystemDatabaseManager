b8c2bbf3961603d91f4a7cec2f001018ace4d067
Yefri Gonzalez <yefrig00@gmail.com>
2023-04-22 23:03:41 -0600

    initial commit

---
+++ src/main/java/com/yefrig/databasemanagementsystem/DataBaseManagementSystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.yefrig.databasemanagementsystem;
+
+import com.yefrig.databasemanagementsystem.front.StartJFrame;
+
+/**
+ *
+ * @author yefri
+ */
+public class DataBaseManagementSystem {
+
+    public static void main(String[] args) {
+        StartJFrame w=new StartJFrame();
+        w.setVisible(true);
+    }
+}
